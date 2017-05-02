cd4cf206891c115b175645ea0cfd813cbae36ea2
gakf <kfgan3@gmail.com>
2017-05-02 11:39:22 +0800

    master/details add ts change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr Nice'},
+  {id: 12, name: 'Superman'},
+  {id: 13, name: 'Batman'}
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
